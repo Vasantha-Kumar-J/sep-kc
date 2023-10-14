07d0621b57ddffa2b5e6b565010066451411c1b3
mahexan <mahesan.periyasamy@solitontech.com>
2023-10-14 13:35:44 +0530

    docs: add documentation for GenerateReport method

--- EmployeeManager/ReportGenerator.cs
+++ EmployeeManager/ReportGenerator.cs
@@ -5,6 +5,12 @@
     /// </summary>
     internal class ReportGenerator
     {
+        /// <summary>
+        /// Generates the report for scheduled tasks report in secondary storage.
+        /// </summary>
+        /// <param name="employeesObj">Employees object.</param>
+        /// <param name="tasksObj">Tasks object.</param>
+        /// <param name="scheduler">Scheduler object.</param>
         public void GenerateReport(EmployeeOperations employeesObj, TaskOperations tasksObj, Scheduler scheduler)
         {
             // TODO: Report generation logic.
