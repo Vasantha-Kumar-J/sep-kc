8bea09d5edefb30ff1b5fa12e72d5c2ca0feba79
ranjith-kumar-e <ranith.eswaran@solitontech.com>
2023-10-19 15:58:18 +0530

    feat: Added node to use file reading in coffee.json

--- RanjithKumar/script.js
+++ RanjithKumar/script.js
@@ -1,34 +1,34 @@
 import { displayTotalCups } from "./JavaScripts/uiManipulation.js";
 import { mapEventListenerToInput, startCoffeeMachine } from "./JavaScripts/startSetUp.js";
 import { checkIngredientLevel } from "./JavaScripts/makingProcess.js";
-
 let coffeeJson
 
 /**
  * Function to initialize the event for inputs
  */
 export async function start () {
-  try { // If the local storage is not defined means copy the coffee.json to local storage
-    coffeeJson = JSON.parse(localStorage.getItem('coffee.json'))
-    displayTotalCups(coffeeJson.totalCupsConsumed)
-    startCoffeeMachine(coffeeJson.isProperlyShutdown)
-    mapEventListenerToInput()
-    checkIngredientLevel()
-    updateCoffeeJson()
-  } catch (error) {
-    coffeeJson = await fetch('./coffee.json').then((data) => data.json())
-    window.localStorage.setItem('coffee.json', JSON.stringify(coffeeJson))
-  }
+  coffeeJson = await fetch('/api/v1/').then((data) => data.json())
+  displayTotalCups(coffeeJson.totalCupsConsumed)
+  startCoffeeMachine(coffeeJson.isProperlyShutdown)
+  mapEventListenerToInput()
+  checkIngredientLevel()
 }
 
 /**
  * Function to update the coffee json file
  * @param {boolean} properShutDown machine properly shuted down or not
  */
-export function updateCoffeeJson(properShutDown = false) {
+export async function updateCoffeeJson(properShutDown = false) {
   coffeeJson.isProperlyShutdown = properShutDown
   coffeeJson.totalCupsConsumed = displayTotalCups(0)
-  localStorage.setItem('coffee.json', JSON.stringify(coffeeJson))
+  const response = await fetch('/api/v1/', {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(coffeeJson)
+  })
 }
 
 window.addEventListener('DOMContentLoaded', start)
