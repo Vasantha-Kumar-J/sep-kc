626c685048cad1f5f2b873817353917f09d8d15b
Danush Pandiyarajan <Danush.Pandiyarajan@solitontech.local>
2023-12-20 13:13:05 +0530

    1pm submission

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ const zonetime = document.getElementById('zone-time')
 const ordervaluedisp = document.querySelector('.order-value')
 const saveconfig = document.querySelector('.save-config')
 const runmodeinput = document.getElementById('run-mode-input')
+const rightindicator = document.getElementsByClassName('right-ind')[0]
+const leftindicator = document.getElementsByClassName('left-ind')[0]
+const backindicator = document.getElementsByClassName('back-ind')[0]
+const frontindicator = document.getElementsByClassName('front-ind')[0]
+const seconds = document.getElementById('sec')
 let state =true;
 let jsonData;
 
@@ -14,12 +19,11 @@ let jsonData;
 (async () => {
     const res = await fetch('/data.json')
     jsonData = await res.json()
-    setupEventListeners(jsonData,state)
+    setupEventListeners(jsonData)
     runmode(jsonData)
 })()
 
 function setupEventListeners(jsonData,state) {
-    if(state) return 
     frontsetup.addEventListener('click',()=>{
         clearconfig()
         frontsetup.classList.add('enable')
@@ -60,7 +64,6 @@ function clearconfig() {
 
 function loadconfig(jsonData,name) {
     
-    if(!state) return
     const foundzone = jsonData.zones.find((fzone) => fzone.zone === name)
     
     //display order
@@ -74,8 +77,6 @@ function loadconfig(jsonData,name) {
 
 
 function savezoneconfig(jsonData,name) {
-
-    if(!state) return
     saveconfig.addEventListener('click',()=> {
         const foundRecordindex = jsonData.zones.findIndex(record => record.zone == name);
         jsonData.zones[foundRecordindex].time = zonetime.value
@@ -88,10 +89,65 @@ function savezoneconfig(jsonData,name) {
 }
 
 function runmode(jsonData) {
-    runmodeinput.addEventListener('click',()=>{
+    runmodeinput.addEventListener('click',async ()=>{
         if(runmodeinput.checked) {
             clearconfig()
             state = false
-        }
+            let sec = 1
+            const sortedzones = jsonData.zones.sort((a,b) => a.order - b.order)
+            seconds.innerHTML = sec 
+
+            indicator(sortedzones[0].zone,sortedzones[0].time)
+            const a = setInterval(() => {
+                sec+=1
+                seconds.innerHTML = sec 
+            },1000);
+
+            const b = setInterval(() =>
+            {
+                indicator(sortedzones[1].zone,sortedzones[1].time)
+
+            const c = setInterval(() => {
+                    indicator(sortedzones[2].zone,sortedzones[2].time)
+            }, sortedzones[2].time*1000);
+
+            const d = setInterval(() => {
+                indicator(sortedzones[3].zone,sortedzones[3].time)
+            }, sortedzones[1].time*1000);
+            clearInterval(b)
+            }, sortedzones[2].time*1000);
+        } 
     } )
+}
+
+function indicator(zone,time) {
+    console.log(zone)
+    if(zone === 'front') {
+        frontindicator.classList.add('indicator')
+        setTimeout(()=>{
+            frontindicator.classList.remove('indicator')
+            return
+        },time*1000)
+    }
+    if(zone === 'back') {
+        backindicator.classList.add('indicator')
+        setTimeout(()=>{
+            backindicator.classList.remove('indicator')
+            return
+        },time*1000)
+    }
+    if(zone === 'right') {
+        rightindicator.classList.add('indicator')
+        setTimeout(()=>{
+            rightindicator.classList.remove('indicator')
+            return
+        },time*1000)
+    }
+    if(zone === 'left') {
+        leftindicator.classList.add('indicator')
+        setTimeout(()=>{
+            leftindicator.classList.remove('indicator')
+            return
+        },time*1000)
+    }
 }
